195431890065e1181222b7a0a6e16da3103fa3c2
Tesuji <68612856+KataKatis@users.noreply.github.com>
2023-10-06 20:25:03 +0200

    Final Cinetique.py

--- Cinetique.py
+++ Cinetique.py
@@ -12,23 +12,14 @@ C[0] = 1.000
 k = 0.464
 
 for i in range(N-1):
-    C[i+1] = C[i] - (t[i+1] - t[i]) * k * C[i]  # Nouvelle concentration = concentration * (1 - delta_t * k)
+    C[i+1] = C[i] - (t[i+1] - t[i]) * k * C[i]
 
 plt.plot(t, C)
-plt.xlabel("Temps (h)")
-plt.ylabel("Concentration molaire  du réactif (mol/L)")
-plt.show()
-
 t_mes = [i/2 for i in range(11)] + [6, 8, 10]
 C_mes = [1.000, 0.789, 0.623, 0.499, 0.394, 0.310, 0.249, 0.197, 0.155, 0.124, 0.098, 0.0618, 0.0245, 0.0095]
 plt.plot(t_mes, C_mes, "+", markersize=12)
+
 plt.xlabel("Temps (h)")
 plt.ylabel("Concentration molaire  du réactif (mol/L)")
-plt.show()
 
-
-plt.plot(t, C)
-plt.plot(t_mes, C_mes, "+", markersize=12)
-plt.xlabel("Temps (h)")
-plt.ylabel("Concentration molaire  du réactif (mol/L)")
 plt.show()
