ad88e4bb4204bcb3c64b6f19fdfd6aa2d9cc0dd7
Tesuji <68612856+KataKatis@users.noreply.github.com>
2023-10-06 20:36:40 +0200

    Update for img

--- Cinetique.py
+++ Cinetique.py
@@ -17,9 +17,12 @@ for i in range(N-1):
 plt.plot(t, C)
 t_mes = [i/2 for i in range(11)] + [6, 8, 10]
 C_mes = [1.000, 0.789, 0.623, 0.499, 0.394, 0.310, 0.249, 0.197, 0.155, 0.124, 0.098, 0.0618, 0.0245, 0.0095]
-plt.plot(t_mes, C_mes, "+", markersize=12)
+plt.plot(t_mes, C_mes, "+", markersize=14, color="red")
 
-plt.xlabel("Temps (h)")
-plt.ylabel("Concentration molaire  du réactif (mol/L)")
+plt.title("Évolution de la concentration molaire du réactif en fonction du temps", fontsize=25)
+plt.xlabel("Temps (h)", fontsize=25)
+plt.ylabel("Concentration molaire  du réactif (mol/L)", fontsize=25)
+plt.tick_params(axis='x', labelsize=20)
+plt.tick_params(axis='y', labelsize=20)
 
 plt.show()
